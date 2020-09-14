af43f039415a525689332b2028360dad9f10f012
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2020-09-13 22:57:14 -0500

    rename bootstrap file

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'matrix-strava-bot',
     script: './node_modules/.bin/ts-node',
-    args: 'src/index.ts',
+    args: 'src/bootstrap.ts',
     env: {
       NODE_ENV: 'development',
     },
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,2 @@
-/* eslint-disable no-console */
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-import { startPoll } from './bot';
-import { Settings } from './settings';
-
-let settings: Settings;
-
-// bootstrap
-try {
-  settings = JSON.parse(
-    readFileSync(join(__dirname, '..', 'bot-config.json'), { encoding: 'utf8' })
-  );
-} catch (e) {
-  const errorMsg = 'Failed loading bot-config.json';
-  console.error(errorMsg, e);
-  throw new Error(errorMsg);
-}
-
-process.argv.forEach((arg) => {
-  if (arg.toLowerCase().includes('--dryrun')) {
-    settings.dryRun = true;
-  }
-});
-
-startPoll(settings);
+export * from './bot';
+export * from './settings';
